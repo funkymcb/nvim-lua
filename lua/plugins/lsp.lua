0b83e5b2a7e2ec2b16861425bd809e6f8fca7919
Yannick Peter <yannick.peter@empiriecom.com>
2023-12-25 10:32:47 +0100

    enable templ support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -27,7 +27,7 @@ return {
 
         require('mason').setup({})
         require('mason-lspconfig').setup({
-            ensure_installed = { 'lua_ls', 'yamlls', 'tsserver', 'terraformls' },
+            ensure_installed = { 'lua_ls', 'yamlls', 'tsserver', 'terraformls', 'templ' },
             automatic_installation = true
         })
 
@@ -47,20 +47,13 @@ return {
                             client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
                                 Lua = {
                                     runtime = {
-                                        -- Tell the language server which version of Lua you're using
-                                        -- (most likely LuaJIT in the case of Neovim)
                                         version = 'LuaJIT'
                                     },
-                                    -- Make the server aware of Neovim runtime files
                                     workspace = {
                                         checkThirdParty = false,
                                         library = {
                                             vim.env.VIMRUNTIME
-                                            -- "${3rd}/luv/library"
-                                            -- "${3rd}/busted/library",
                                         }
-                                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                                        -- library = vim.api.nvim_get_runtime_file("", true)
                                     }
                                 }
                             })
