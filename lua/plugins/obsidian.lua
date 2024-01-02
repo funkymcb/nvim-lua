ab8d232894b19869eedb4857bd6bc11989792a36
funkymcb <yannick.peter92@gmail.com>
2024-01-02 14:30:28 +0100

    fix obsidian template handling

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -2,6 +2,11 @@ return {
 	"epwalsh/obsidian.nvim",
 	config = true,
 	opts = {
+		templates = {
+			subdir = "Hacking/templates",
+			date_format = "%d.%m.%Y",
+			time_format = "%H:%M",
+		},
 		workspaces = {
 			{
 				name = "42",
