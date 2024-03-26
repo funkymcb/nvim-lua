3bddb690a0c5d3303043577e3f77a750842a28f3
Yannick Peter <yannick.peter@empiriecom.com>
2024-03-26 15:27:12 +0100

    fix dap und rest plugins

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,11 +1,23 @@
 return {
-	"rest-nvim/rest.nvim",
-	dependencies = { { "nvim-lua/plenary.nvim" } },
-	config = function()
-		require("rest-nvim").setup({
-			result_split_horizontal = true,
-		})
-		vim.keymap.set("n", "<space>rx", "<Plug>RestNvim<CR>", {})
-		vim.keymap.set("n", "<space>rp", "<Plug>RestNvimPreview<CR>", {})
-	end,
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1000,
+		config = true,
+	},
+	{
+		"rest-nvim/rest.nvim",
+		dependencies = { "luarocks.nvim" },
+		ft = "http",
+		config = function()
+			require("rest-nvim").setup({
+				result = {
+					split = {
+						horizontal = true,
+					},
+				},
+			})
+			vim.keymap.set("n", "<space>rx", "<Plug>RestNvim<CR>", {})
+			vim.keymap.set("n", "<space>rp", "<Plug>RestNvimPreview<CR>", {})
+		end,
+	},
 }
