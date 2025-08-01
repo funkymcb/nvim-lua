089beeb47bb72f5c03600e87084421ba537ad9b2
Yannick Peter <yannick.peter@empiriecom.com>
2025-08-01 14:54:25 +0200

    cleanup before rework

--- lua/plugins/signature.lua
+++ lua/plugins/signature.lua
@@ -1,17 +1,17 @@
 return {
-	"ray-x/lsp_signature.nvim",
-	event = "VeryLazy",
-	opts = {
-		bind = true,
-		hint_prefix = "💡 ",
-		hi_parameter = "LspSignatureActiveCustomParameter",
-		transparency = 50,
-		handler_opts = {
-			border = "rounded",
-		},
-	},
-	config = function(_, opts)
-		vim.cmd([[highlight LspSignatureActiveCustomParameter guifg=#31C236 gui=nocombine]])
-		require("lsp_signature").setup(opts)
-	end,
+    "ray-x/lsp_signature.nvim",
+    event = "InsertEnter",
+    opts = {
+        bind = true,
+        hint_prefix = "💡 ",
+        hi_parameter = "LspSignatureActiveCustomParameter",
+        transparency = 50,
+        handler_opts = {
+            border = "rounded",
+        },
+    },
+    config = function(_, opts)
+        vim.cmd([[highlight LspSignatureActiveCustomParameter guifg=#31C236 gui=nocombine]])
+        require("lsp_signature").setup(opts)
+    end,
 }
