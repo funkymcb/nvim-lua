d5b251497bebfb0a77ed76f5d6647c6008e5e696
Yannick Peter <yannick.peter@empiriecom.com>
2025-09-11 13:20:52 +0200

    add goimports

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,7 +4,7 @@ return {
 		opts = {
 			formatters_by_ft = {
 				lua = { "stylua" },
-				go = { "gofumpt" },
+				go = { "gofumpt", "goimports" },
 			},
 			format_on_save = {
 				timeout_ms = 500,
