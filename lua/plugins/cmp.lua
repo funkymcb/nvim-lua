910123e605a6322e1afffb989054d764b5ffdce5
Yannick Peter <yannick.peter@empiriecom.com>
2026-02-05 15:21:10 +0100

    correctly remap copilot tab

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -27,7 +27,6 @@ return {
 				["<C-d>"] = cmp.mapping.scroll_docs(4),
 				["<Left>"] = cmp.mapping.abort(),
 				["<C-c>"] = cmp.mapping.close(),
-				["<C-l>"] = cmp.mapping.complete(),
 				["<CR>"] = cmp.mapping.confirm({
 					behavior = cmp.ConfirmBehavior.Insert,
 					select = true,
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -10,6 +10,27 @@ return {
 				desc = "Open Copilot Panel",
 			},
 		},
+		config = function()
+			vim.keymap.set("i", "<C-l>", 'copilot#Accept("<CR>")', {
+				expr = true,
+				replace_keycodes = false,
+			})
+			vim.g.copilot_no_tab_map = true
+
+			-- enable copilot for specific filetypes
+			vim.g.copilot_filetypes = {
+				["*"] = false,
+				["lua"] = true,
+				["python"] = true,
+				["javascript"] = true,
+				["typescript"] = true,
+				["go"] = true,
+				["rust"] = true,
+				["java"] = true,
+				["c"] = true,
+				["cpp"] = true,
+			}
+		end,
 	},
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
